25d1390b1a018dbdefe49209b7c93adddc064c89
cowboycrackie <112209004+cowboycrackie@users.noreply.github.com>
2022-11-01 17:32:18 +0100

    home

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const navMenu = document.querySelector('nav-menu');
-const navToggle = document.querySelector('nav-toggle');
-const navClose = document.querySelector('nav-close');
+//menu show and hidden
+const navMenu = document.querySelector('#nav-menu');
+const navToggle = document.querySelector('#nav-toggle');
+const navClose = document.querySelector('#nav-close');
 
 if (navToggle) {
   navToggle.addEventListener('click', () => {
@@ -13,3 +14,12 @@ if (navClose) {
     navMenu.classList.remove('show-menu');
   });
 }
+
+//remove menu
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
