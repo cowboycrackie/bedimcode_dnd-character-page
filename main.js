7305ecc08a8206daa9a822743bbec3ede70ba8b0
cowboycrackie <112209004+cowboycrackie@users.noreply.github.com>
2022-11-06 13:56:00 +0100

    services

--- main.js
+++ main.js
@@ -63,3 +63,26 @@ tabs.forEach((tab) => {
     tab.classList.add('qualification__active');
   });
 });
+
+//modal
+const modalViews = document.querySelectorAll('.services__modal'),
+  modalBtns = document.querySelectorAll('.services__button'),
+  modalCloses = document.querySelectorAll('.services__modal-close');
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add('active-modal');
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener('click', () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose, i) => {
+  modalClose.addEventListener('click', () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal');
+    });
+  });
+});
