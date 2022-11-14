4fa6ddc80c1f0149c3d3bc131a905dd5ef586b63
cowboycrackie <112209004+cowboycrackie@users.noreply.github.com>
2022-11-14 16:32:31 +0100

    js functionality for scroll events

--- main.js
+++ main.js
@@ -119,3 +119,45 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
     },
   },
 });
+
+// active link sections on scroll
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
+
+//change header bg on scroll
+function scrollHeader() {
+  const nav = document.getElementById('header');
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 200) nav.classList.add('scroll-header');
+  else nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+//show scroll button on scroll
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
